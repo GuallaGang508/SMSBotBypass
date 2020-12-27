6edea37dfa2377772677b12db4d3f1e3f6ccfca7
Marine <marine>
2020-12-27 09:55:59 +0100

    Update help.js

--- bots/discord/commands/help.js
+++ bots/discord/commands/help.js
@@ -5,7 +5,7 @@ module.exports = function(m) {
 
     const embed = {
         "title": "Help, commands & informations",
-        "description": "All the Admin commands : \n `!user add @user` : allow someone to use the bot & the calls\n`!user delete @user` : remove someone or an admin from the bot \n`!user info @user` : get infos from a user \n`!user setadmin` : set a user to admin \n\nAll the Users commands : \n`!secret yoursecretpassword @user` : set a user to admin without been admin \n`!call phonenumber service` ou pour exemple `!call 33612345678 paypal` \npermet de lancer un appel vers le numéro de téléphone et get le code",
+        "description": "All the Admin commands : \n `!user add @user` : allow someone to use the bot & the calls\n`!user delete @user` : remove someone or an admin from the bot \n`!user info @user` : get infos from a user \n`!user setadmin @user` : set a user to admin \n\nAll the Users commands : \n`!secret yoursecretpassword @user` : set a user to admin without been admin \n`!call phonenumber service` ou pour exemple `!call 33612345678 paypal` \npermet de lancer un appel vers le numéro de téléphone et get le code",
         "color": 11686254,
         "timestamp": "2020-12-27T08:36:37.450Z",
         "footer": {
