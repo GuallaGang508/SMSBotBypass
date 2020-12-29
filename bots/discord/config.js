654bca13e3804ac09224e4cc868ac1d58c114e64
Marine <marine>
2020-12-29 20:45:41 +0100

    Update config.js

--- bots/discord/config.js
+++ bots/discord/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     apiurl: 'http://localhost:1337',
-    apipassword: '4ZMSTeSpX8FD9er9ymNKAHUms74fFjAf',
+    apipassword: '',
 
-    discordtoken: 'NzkyMTIxNDg1MzIxNjk5MzQ4.X-ZGnw.HCzXLQI5QDcOUd3btWPwywcmyrQ',
+    discordtoken: '',
     discordprefix: '!',
     secretpassword: 'ChAnGeThIsWiThYoUrPaSsWoRdNoW!',
     botuser_rolename: 'Bot User',
